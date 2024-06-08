139042bf21c6d5218ebacda256efc1045ea5b835
‘dreamer’ <bityuanping@vip.qq.com>
2024-06-08 18:59:39 +0800

    Changes

--- faceformer.py
+++ faceformer.py
@@ -152,7 +152,9 @@ class Faceformer(nn.Module):
             else:
                 vertice_input = self.PPE(vertice_emb)
 
+            # biased casual mh self-attention
             tgt_mask = self.biased_mask[:, :vertice_input.shape[1], :vertice_input.shape[1]].clone().detach().to(device=self.device)
+            # biased cross modal mh attention
             memory_mask = enc_dec_mask(self.device, self.dataset, vertice_input.shape[1], hidden_states.shape[1])
             vertice_out = self.transformer_decoder(vertice_input, hidden_states, tgt_mask=tgt_mask, memory_mask=memory_mask)
             vertice_out = self.vertice_map_r(vertice_out)
